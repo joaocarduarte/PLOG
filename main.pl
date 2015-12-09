d9b62121f0825979bb16e8216ebb8c665957d6b8
Eduardo Reis <up201303832@gcloud.fe.up.pt>
2015-12-09 16:24:17 +0000

    cenas

--- main.pl
+++ main.pl
@@ -3,7 +3,7 @@
 :- use_module(library(clpfd)).
 :- use_module(library(samsort)).
 
-config_file('/Users/veryc/Desktop/PLOP/TP2(git)/PLOG/config.pl').
+config_file('/Users/Eduardo Reis/Documents/PLOG/PLOG/config.pl').
 
 read_config :-
     config_file(F),
@@ -19,21 +19,18 @@ read_file(Stream, [X|L]) :-
     assert(X),
     read_file(Stream, L).
 
-mais_barato(ListPreco) :- 
-	sopa(A,P1),
-	prato(B,P2),
-	sobremesa(C,P3),
-	append([], [P1, P2, P3], ListPreco).
+listas(ListPreco) :-
+        sopa(A, P1),
+        prato(B, P2),
+        sobremesa(C, P3),
+        append([], [P1, P2, P3], ListPreco).
 
-mais_baratinho([X|Y]) :-
-	Zero is 10,
-	Zero < X,
-	write(X);
-	Zero > X,
-	write(Y).
-
+mais_baratinho(Lista, W) :-
+        samsort(Lista, [X|Y]),
+        W is X.
 
 teste :-
 	read_config,
-	mais_barato(ListPreco),
-	mais_baratinho(ListPreco).
+	listas(ListPreco),
+	mais_baratinho(ListPreco, W),
+        write(W).
